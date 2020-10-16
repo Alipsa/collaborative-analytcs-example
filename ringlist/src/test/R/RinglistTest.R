61580e0d5a6d7b9e0393dcbfb8163f4eaa4a4c0c
per <per@alipsa.se>
2020-10-16 20:44:33 +0200

    bump versions and add simple bootstrap styling

--- ringlist/src/test/R/RinglistTest.R
+++ ringlist/src/test/R/RinglistTest.R
@@ -1,1 +1,7 @@
 library('hamcrest')
+
+assertThat(nrow(customers), equalTo(5))
+assertThat(nrow(validCustomers), equalTo(3))
+
+#inout$View(customers, "customers")
+#inout$View(validCustomers, "validCustomers")
